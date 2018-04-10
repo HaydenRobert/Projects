d7d17ef76ea8c0c0d03e05bca70ced5c2f786107
jc432983 <hrobe125@gmail.com>
2018-04-10 13:01:22 +1000

    add scratch exercises of prac_07

---
+++ prac_07/guitar_test.py
@@ -0,0 +1,15 @@
+""""""
+
+from prac_07.Guitar import Guitar
+
+
+def main():
+    """"""
+    first_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    print("{} get_age() - Expected 96. Got {}".format(first_guitar.name, first_guitar.get_age()))
+    another_guitar = Guitar("Another guitar", 2012, 16035.40)
+    print("{} get_age() - Expected 6. Got {}".format(another_guitar.name, another_guitar.get_age()))
+
+
+
+main()
